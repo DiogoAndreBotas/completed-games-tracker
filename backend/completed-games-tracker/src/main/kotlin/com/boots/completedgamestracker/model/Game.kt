2dac31e0bfecef480f5a03eef756a1f529198ea5
DiogoAndreBotas <gamebreak24@gmail.com>
2022-04-14 16:14:58 +0100

    Add more fields to Game DTO

--- backend/completed-games-tracker/src/main/kotlin/com/boots/completedgamestracker/model/Game.kt
+++ backend/completed-games-tracker/src/main/kotlin/com/boots/completedgamestracker/model/Game.kt
@@ -8,5 +8,12 @@ import org.springframework.data.mongodb.core.mapping.Document
 class Game (
     @Id
     val id: String = ObjectId().toString(),
-    val name: String
+    val name: String,
+    val platform: String = "",
+    val hoursPlayed: String = "",
+    val releaseDate: String = "",
+    val steamStoreLink: String = "",
+    val developer: String = "",
+    val publisher: String = "",
+    val image: String = ""
 )
